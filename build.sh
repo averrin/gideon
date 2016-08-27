e9324ee4ba3c4134067f180de9ff110a7d27781e
averrin <averrin@gmail.com>
2016-08-27 13:33:58 +0300

    fix path

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 export GOPATH=$(pwd)
+export PATH=$PATH:/usr/local/go/bin
 rev=$(git log --pretty=format:'' | wc -l)
 go build -ldflags "-s -X main.VERSION=0.1.$rev" -o ./gideon ./*.go;
 echo "Build completed"
