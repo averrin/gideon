ef548431f278f1a83d0d226ea0aad6ce7c8a1dfa
averrin <averrin@gmail.com>
2016-08-28 20:44:13 +0300

    update echoes

--- update.sh
+++ update.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 cd ~/gideon_src;
+echo 'Update sources'
 git pull;
 cd ~/gideon_src/src/github.com/averrin/shodan;
+echo 'Update Shodan'
 git pull;
 cd -;
 cd ~/gideon_src/src/github.com/averrin/seker;
+echo 'Update Seker'
 git pull;
 cd -;
+echo 'Building...'
 ./build.sh
